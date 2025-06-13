e2c3b891ab92e5e1679c652282ab41f780fd72d7
om borwadkar <omborwadkar@gmail.com>
2025-06-13 13:51:37 +0530

    Updated the route

--- app.js
+++ app.js
@@ -69,9 +69,9 @@ const sessionOptions = {
     }
 };
 
-// app.get("/" , (req , res) => {
-//     res.send("Working");
-// })
+app.get("/" , (req , res) => {
+    res.redirect("/listings");
+})
 
 app.use(session(sessionOptions));
 app.use(flash());
